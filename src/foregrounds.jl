22ef4a602d3a57a2528016e51cb783201ce6b6fd
Marco Bonici <bonici.marco@gmail.com>
2023-02-23 02:33:39 +0100

    Added GalacticDust and CIBClustering

--- src/foregrounds.jl
+++ src/foregrounds.jl
@@ -20,6 +20,34 @@ function DustFreqScaling(β, Tdust, ν0, ν_eff)
     return fdust
 end
 
+function GalacticDust(pow_at_80, α, β, ν1, ν2, SPT3G_windows_lmax, T_galdust, ν_0_galdust)
+
+        # Grab ells helper (1-3200)
+        ells = Array(1:SPT3G_windows_lmax)
+
+        # Calculate and add galactic dust power
+        Dl_galdust =  (ells ./ 80) .^ (α + 2.0) .* (pow_at_80 *
+        DustFreqScaling(β, T_galdust, ν_0_galdust, ν1) *
+        DustFreqScaling(β, T_galdust, ν_0_galdust, ν2))
+
+
+        return Dl_galdust
+end
+
+function CIBClustering(pow_at_3000, α, β, ν1, ν2, z1, z2, SPT3G_windows_lmax, T_CIB,
+    ν0_CIB)
+
+        # Grab ells helper (1-3200)
+        ells = Array(1:SPT3G_windows_lmax)
+
+        # Calculate and add polarised galactic dust power
+        Dl_cib_clustering =  (ells ./ 3000) .^ α .* (pow_at_3000 *
+        DustFreqScaling(β, T_CIB, ν0_CIB, ν1) *
+        DustFreqScaling(β, T_CIB, ν0_CIB, ν2) * sqrt(z1 * z2))
+
+        return Dl_cib_clustering
+end
+
 function tSZFrequencyScaling(ν, ν0, T)
     x0 = Ghz_Kelvin * ν0 / T
     x = Ghz_Kelvin * ν / T
