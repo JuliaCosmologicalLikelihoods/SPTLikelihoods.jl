aba84f13fad8a721c6af3659f514e9686a1d709b
Marco Bonici <bonici.marco@gmail.com>
2025-08-28 01:52:30 +0200

    Updated compute theory

--- src/utils.jl
+++ src/utils.jl
@@ -145,117 +145,67 @@ function compute_theory(DL_TT, DL_TE, DL_EE, κ,#1
     ab_EE = aberration_response(ℓs, ab_coeff, DL_EE.+ssl_EE)
     ν_eff = effective_band_centres
 
-    #TODO probably this HORRIBLE thing can be transformed in a nested for loop
-    #result = zeros(18,3200)
-    #idx = 0
-    #for i in 1:3
-    #for j in i:3
-    #idx+=1
-    #result[3*(i-1)+1] = TT_calculation(ν[i], ν[j])
-    #result[3*(i-1)+2] = TE_calculation(ν[i], ν[j])
-    #result[3*(i-1)+3] = EE_calculation(ν[i], ν[j])
-    #end
-    #end
-    #for i in 1:18
-    #@views model_matrix[i,:]  = window[:,i,:]*result[i,:]
-    #end
-    TT_90_90   = (DL_TT.+ssl_TT.+ab_TT.+TT_foregrounds(D_TT_90_90, A_80_cirrus, α_cirrus, β_cirrus,
-    ν_eff[1,1], ν_eff[1,1], A_80_cib, α_cib, β_cib, ν_eff[3,1], ν_eff[3,1], 1., 1., A_tSZ,
-    ν_eff[5,1], ν_eff[5,1], ξ_tsz_CIB, A_kSZ, ℓs)) ./
-    cross_calibration_mean(cal_T_90, cal_T_90, cal_T_90, cal_T_90)
+    # Channel index order: 1→90 GHz, 2→150 GHz, 3→220 GHz
+    pairs = ((1,1), (1,2), (1,3), (2,2), (2,3), (3,3))
 
-    TT_90_150  = (DL_TT.+ssl_TT.+ab_TT.+TT_foregrounds(D_TT_90_150, A_80_cirrus, α_cirrus, β_cirrus,
-    ν_eff[1,1], ν_eff[1,2], A_80_cib, α_cib, β_cib, ν_eff[3,1], ν_eff[3,2], 1., 1., A_tSZ,
-    ν_eff[5,1], ν_eff[5,2], ξ_tsz_CIB, A_kSZ, ℓs)) ./
-    cross_calibration_mean(cal_T_90, cal_T_150, cal_T_90, cal_T_150)
+    # Base spectra (identical across frequency pairs for a given type)
+    base_TT = DL_TT .+ ssl_TT .+ ab_TT
+    base_TE = DL_TE .+ ssl_TE .+ ab_TE
+    base_EE = DL_EE .+ ssl_EE .+ ab_EE
 
-    TT_90_220  = (DL_TT.+ssl_TT.+ab_TT.+TT_foregrounds(D_TT_90_220, A_80_cirrus, α_cirrus, β_cirrus,
-    ν_eff[1,1], ν_eff[1,3], A_80_cib, α_cib, β_cib, ν_eff[3,1], ν_eff[3,3], 1., 1., A_tSZ,
-    ν_eff[5,1], ν_eff[5,3], ξ_tsz_CIB, A_kSZ, ℓs)) ./
-    cross_calibration_mean(cal_T_90, cal_T_220, cal_T_90, cal_T_220)
+    # Frequency lookups (non-mutating views or plain indexing are both fine)
+    ν_gc_T = @view ν_eff[1, :]   # TT galactic-dust freqs
+    ν_E    = @view ν_eff[2, :]   # E-channel freqs (EE/TE dust)
+    ν_cib  = @view ν_eff[3, :]   # CIB freqs
+    ν_tsz  = @view ν_eff[5, :]   # tSZ freqs
 
-    TT_150_150 = (DL_TT.+ssl_TT.+ab_TT.+TT_foregrounds(D_TT_150_150, A_80_cirrus, α_cirrus, β_cirrus,
-    ν_eff[1,2], ν_eff[1,2], A_80_cib, α_cib, β_cib, ν_eff[3,2], ν_eff[3,2], 1., 1., A_tSZ,
-    ν_eff[5,2], ν_eff[5,2], ξ_tsz_CIB, A_kSZ, ℓs)) ./
-    cross_calibration_mean(cal_T_150, cal_T_150, cal_T_150, cal_T_150)
+    # Shot-noise amplitudes (symmetric 3×3)
+    D_TT = [D_TT_90_90   D_TT_90_150  D_TT_90_220;
+            D_TT_90_150  D_TT_150_150 D_TT_150_220;
+            D_TT_90_220  D_TT_150_220 D_TT_220_220]
 
-    TT_150_220 = (DL_TT.+ssl_TT.+ab_TT.+TT_foregrounds(D_TT_150_220, A_80_cirrus, α_cirrus, β_cirrus,
-    ν_eff[1,2], ν_eff[1,3], A_80_cib, α_cib, β_cib, ν_eff[3,2], ν_eff[3,3], 1., 1., A_tSZ,
-    ν_eff[5,2], ν_eff[5,3], ξ_tsz_CIB, A_kSZ, ℓs)) ./
-    cross_calibration_mean(cal_T_150, cal_T_220, cal_T_220, cal_T_150)
+    D_EE = [D_EE_90_90   D_EE_90_150  D_EE_90_220;
+            D_EE_90_150  D_EE_150_150 D_EE_150_220;
+            D_EE_90_220  D_EE_150_220 D_EE_220_220]
 
-    TT_220_220 = (DL_TT.+ssl_TT.+ab_TT.+TT_foregrounds(D_TT_220_220, A_80_cirrus, α_cirrus, β_cirrus,
-    ν_eff[1,3], ν_eff[1,3], A_80_cib, α_cib, β_cib, ν_eff[3,3], ν_eff[3,3], 1., 1., A_tSZ,
-    ν_eff[5,3], ν_eff[5,3], ξ_tsz_CIB, A_kSZ, ℓs)) ./
-    cross_calibration_mean(cal_T_220, cal_T_220, cal_T_220, cal_T_220)
+    # Calibration means (precompute with comprehensions; no mutation)
+    cal_T = (cal_T_90, cal_T_150, cal_T_220)
+    cal_E = (cal_E_90, cal_E_150, cal_E_220)
 
+    cal_mean_TT = [cross_calibration_mean(cal_T[i], cal_T[j], cal_T[j], cal_T[i]) for i=1:3, j=1:3]
+    cal_mean_TE = [cross_calibration_mean(cal_T[i], cal_E[j], cal_T[j], cal_E[i]) for i=1:3, j=1:3]
+    cal_mean_EE = [cross_calibration_mean(cal_E[i], cal_E[j], cal_E[j], cal_E[i]) for i=1:3, j=1:3]
 
-    EE_90_90   = (DL_EE.+ssl_EE.+ab_EE.+EE_foregrounds(D_EE_90_90, A_80_EE, α_EE, β_EE,
-                                       ν_eff[2,1], ν_eff[2,1], ℓs)) ./
-                                    cross_calibration_mean(cal_E_90, cal_E_90, cal_E_90, cal_E_90)
+    # Foreground builders matching your existing signatures (pure, out-of-place)
+    tt_fore(i,j) = TT_foregrounds(
+        D_TT[i,j],
+        A_80_cirrus, α_cirrus, β_cirrus, ν_gc_T[i], ν_gc_T[j],
+        A_80_cib,    α_cib,    β_cib,    ν_cib[i],  ν_cib[j], 1.0, 1.0,
+        A_tSZ,       ν_tsz[i], ν_tsz[j],
+        ξ_tsz_CIB,   A_kSZ,    ℓs
+    )
 
-    EE_90_150  = (DL_EE.+ssl_EE.+ab_EE.+EE_foregrounds(D_EE_90_150, A_80_EE, α_EE, β_EE,
-                                       ν_eff[2,1], ν_eff[2,2], ℓs)) ./
-                                    cross_calibration_mean(cal_E_90, cal_E_150, cal_E_90, cal_E_150)
+    ee_fore(i,j) = EE_foregrounds(
+        D_EE[i,j],
+        A_80_EE, α_EE, β_EE, ν_E[i], ν_E[j], ℓs
+    )
 
-    EE_90_220  = (DL_EE.+ssl_EE.+ab_EE.+EE_foregrounds(D_EE_90_220, A_80_EE, α_EE, β_EE,
-                                       ν_eff[2,1], ν_eff[2,3], ℓs)) ./
-                                    cross_calibration_mean(cal_E_90, cal_E_220, cal_E_90, cal_E_220)
+    te_fore(i,j) = TE_foregrounds(
+        A_80_TE, α_TE, β_TE, ν_E[i], ν_E[j], ℓs
+    )
 
-    EE_150_150 = (DL_EE.+ssl_EE.+ab_EE.+EE_foregrounds(D_EE_150_150, A_80_EE, α_EE, β_EE,
-                                       ν_eff[2,2], ν_eff[2,2], ℓs)) ./
-                                    cross_calibration_mean(cal_E_150, cal_E_150, cal_E_150, cal_E_150)
+    # Final spectra per pair (pure, broadcasted arithmetic)
+    tt_spec(i,j) = (base_TT .+ tt_fore(i,j)) ./ cal_mean_TT[i,j]
+    te_spec(i,j) = (base_TE .+ te_fore(i,j)) ./ cal_mean_TE[i,j]
+    ee_spec(i,j) = (base_EE .+ ee_fore(i,j)) ./ cal_mean_EE[i,j]
 
-    EE_150_220 = (DL_EE.+ssl_EE.+ab_EE.+EE_foregrounds(D_EE_150_220, A_80_EE, α_EE, β_EE,
-                                       ν_eff[2,2], ν_eff[2,3], ℓs)) ./
-                                    cross_calibration_mean(cal_E_150, cal_E_220, cal_E_220, cal_E_150)
-
-    EE_220_220 = (DL_EE.+ssl_EE.+ab_EE.+EE_foregrounds(D_EE_220_220, A_80_EE, α_EE, β_EE,
-                                       ν_eff[2,3], ν_eff[2,3], ℓs)) ./
-                                    cross_calibration_mean(cal_E_220, cal_E_220, cal_E_220, cal_E_220)
-
-
-    TE_90_90   = (DL_TE.+ssl_TE.+ab_TE.+TE_foregrounds(A_80_TE, α_TE, β_TE,
-                                       ν_eff[2,1], ν_eff[2,1], ℓs)) ./
-                                    cross_calibration_mean(cal_T_90, cal_E_90, cal_T_90, cal_E_90)
-
-    TE_90_150  = (DL_TE.+ssl_TE.+ab_TE.+TE_foregrounds(A_80_TE, α_TE, β_TE,
-                                       ν_eff[2,1], ν_eff[2,2], ℓs)) ./
-                                    cross_calibration_mean(cal_T_90, cal_E_150, cal_T_150, cal_E_90)
-
-    TE_90_220  = (DL_TE.+ssl_TE.+ab_TE.+TE_foregrounds(A_80_TE, α_TE, β_TE,
-                                       ν_eff[2,1], ν_eff[2,3], ℓs)) ./
-                                    cross_calibration_mean(cal_T_90, cal_E_220, cal_T_220, cal_E_90)
-
-    TE_150_150 = (DL_TE.+ssl_TE.+ab_TE.+TE_foregrounds(A_80_TE, α_TE, β_TE,
-                                       ν_eff[2,2], ν_eff[2,2], ℓs)) ./
-                                    cross_calibration_mean(cal_T_150, cal_E_150, cal_T_150, cal_E_150)
-
-    TE_150_220 = (DL_TE.+ssl_TE.+ab_TE.+TE_foregrounds(A_80_TE, α_TE, β_TE,
-                                       ν_eff[2,2], ν_eff[2,3], ℓs)) ./
-                                    cross_calibration_mean(cal_T_150, cal_E_220, cal_T_220, cal_E_150)
-
-    TE_220_220 = (DL_TE.+ssl_TE.+ab_TE.+TE_foregrounds(A_80_TE, α_TE, β_TE,
-                                       ν_eff[2,3], ν_eff[2,3], ℓs)) ./
-                                    cross_calibration_mean(cal_T_220, cal_E_220, cal_T_220, cal_E_220)
-
-    pure_theory = [
-      TT_90_90, TE_90_90, EE_90_90,
-      TT_90_150, TE_90_150, EE_90_150,
-      TT_90_220, TE_90_220, EE_90_220,
-      TT_150_150, TE_150_150, EE_150_150,
-      TT_150_220, TE_150_220, EE_150_220,
-      TT_220_220, TE_220_220, EE_220_220
-    ]
+    # Assemble in the exact original ordering: TT_.., TE_.., EE_.. for each pair
+    pure_theory = [f(i,j) for (i,j) in pairs for f in (tt_spec, te_spec, ee_spec)]
 
     model_matrix = stack(1:18) do i
        @view(window[:,:,i])' * pure_theory[i]
     end
 
-    #residuals = bandpowers .- model_matrix
-
-    #vec_residuals = vcat([residuals[i, spec_bin_min[i]:spec_bin_max[i]] for i in 1:18]...)
-    #dbs = vcat([model_matrix[i, spec_bin_min[i]:spec_bin_max[i]] for i in 1:18]...)
     return model_matrix'
 end
 
